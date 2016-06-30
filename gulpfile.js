e2aff62ebe43ed08e41f8dfe9b265cbb268c06b8
Simone <simone.cogno@gmail.com>
2016-06-30 22:18:57 +0200

    Modifica

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@ var concat = require('gulp-concat');
 var less= require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 
+var config = {
+    bowerDir: './bower_components'
+}
+
 gulp.task('scripts', ['bootstrapjs'], function() {
     return gulp.src(['./js/jquery.js',
-                     './bower_components/bootstrap_less/',
-                     './js/**/*.js',
-                     './js/*.js'])
+                     './bower_components/bootstrap_less/js/',
+                     './js/raphael.min.js',
+                     './js/morris-data.js',
+                     './js/morris.js'])
+        .pipe(sourcemaps.init())
         .pipe(concat('all.js'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/'));
 });
+
 gulp.task('bootstrapjs', function() {
     return gulp.src(['./bower_components/bootstrap_less/js/*.js'])
         .pipe(concat('bootstrap.js'))
@@ -18,7 +26,10 @@ gulp.task('bootstrapjs', function() {
 });
 
 gulp.task('css', function() {
-    return gulp.src(['./css/**/*.css', './bower_components/bootstrap-less/bootstrap.css'])
+    return gulp.src([,
+        './bower_components/bootstrap-less/bootstrap.css',
+        './bower_components/font-awesome/css/font-awesome.css',
+        './css/**/*.css'])
         .pipe(concat('all.css'))
         .pipe(gulp.dest('./dist/'));
 });
@@ -30,10 +41,14 @@ gulp.task('bootstrap-less', function(){
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./bower_components/bootstrap-less/'));
 });
-
-gulp.task('sass', ['bootstrap-less', 'css'], function() {
+gulp.task('icons', function() {
+        return gulp.src('./bower_components/font-awesome/fonts/*')
+        .pipe(gulp.dest('./fonts'));
+});
+gulp.task('sass', ['bootstrap-less', 'css', 'icons'], function() {
 
 });
+
 gulp.task('default',['sass', 'scripts'], function() {
   // place code for your default task here
 });
